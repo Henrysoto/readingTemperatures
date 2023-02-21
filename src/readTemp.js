e03f9aea296076d0bfb1d63f9d79eaf864ad5bcb
Henrysoto <rtcwpub@gmail.com>
2023-02-21 15:30:15 +0100

    added comment

--- src/readTemp.js
+++ src/readTemp.js
@@ -95,19 +95,7 @@ window.addEventListener('DOMContentLoaded', () => {
           });
         });
 
-        // Chart.js custom background
-        // const plugin = {
-        //   id: 'customCanvasBackgroundColor',
-        //   beforeDraw: (chart, args, options) => {
-        //     const {ctx} = chart;
-        //     ctx.save();
-        //     ctx.globalCompositeOperation = 'destination-over';
-        //     ctx.fillStyle = options.color || '#99ffff';
-        //     ctx.fillRect(0, 0, chart.width, chart.height);
-        //     ctx.restore();
-        //   }
-        // };
-
+        // Global chart color settings
         Chart.defaults.backgroundColor = '#CCBFAB';
         Chart.defaults.borderColor = '#CCBFAB';
         Chart.defaults.color = '#FFF';
